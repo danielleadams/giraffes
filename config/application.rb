1b8bd22fff95925f66ab5c50b1d466a20982b2ac
Danielle Adams <danielle.adams@heroku.com>
2019-06-07 17:42:07 -0400

    test

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+ENV.fetch("KEY_VALUE")
+
 module Zoo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
